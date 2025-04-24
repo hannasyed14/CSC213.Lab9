ceaa3ab88d6fb3d053da8a25ef6bd7b579e533b1
hannasyed14 <syed4@my.canisius.edu>
2025-04-24 10:38:37 -0400

    Completed Lab 9 - Decryption tasks

--- DecryptionLab.java
+++ DecryptionLab.java
@@ -1,42 +1,50 @@
 import java.lang.reflect.Method;
 
 public class DecryptionLab {
-    public static void main(String[] args) throws Exception {
-        Class<?> clazz = Class.forName("MysteryToolbox");
+	public static void main(String[] args) throws Exception {
+		Class<?> clazz = Class.forName("MysteryToolbox");
 
-        Object toolbox = clazz.getDeclaredConstructor().newInstance();
+		Object toolbox = clazz.getDeclaredConstructor().newInstance();
 
-        // List methods
-        Method[] methods = clazz.getDeclaredMethods();
+		// List methods
+		Method[] methods = clazz.getDeclaredMethods();
 
-        for (Method m : methods) {
-            if (m.getParameterCount() == 1 && m.getParameterTypes()[0] == String.class) {
-                String methodName = m.getName();
-		System.out.println(methodName);
-		String key;
-		switch (methodName) {
-    			case "hammer":
-        			key = "aRTyWEy3RngEqgqq";
-        			break;
-    			case "screwdriver":
-        			key = "kNYWIeNpFYlAOLPK";
-        			break;
-    			case "wrench":
-        			key = "XI4i0C09PZ4rk6fY";
-        			break;
-    			case "glue":
-        			key = "zYeoyfW2agYfeUmi";
-        			break;
-    			default:
-        			key = "";
+		for (Method m : methods) {
+			if (m.getParameterCount() == 1 && m.getParameterTypes()[0] == String.class) {
+				String methodName = m.getName();
+				String key;
+				switch (methodName) {
+					case "hammer":
+						key = "aRTyWEy3RngEqgqq";
+						break;
+					case "screwdriver":
+						key = "kNYWIeNpFYlAOLPK";
+						break;
+					case "wrench":
+						key = "XI4i0C09PZ4rk6fY";
+						break;
+					case "glue":
+						key = "zYeoyfW2agYfeUmi";
+						break;
+					default:
+						key = "";
+						break;
+				}
+
+				m.setAccessible(true); // not needed here but safe practice
+				// Invoke the method to get the decrypted version
+				Object result = m.invoke(toolbox, key);
+				// Clean the result to remove the repeating
+				String cleanResult = result.toString().replaceFirst("🧰 " + methodName + ": ", "");
+				System.out.println("🧰 " + methodName + ": " + cleanResult);
+			}
 		}
-
-                m.setAccessible(true); // not needed here but safe practice
-                //TODO - you need to invoke the method to get the decrypted version!
-		Object result = "This is not the unencrypted message!  See the TODO above";
-                System.out.println("🧰 " + methodName + ": " + result);
-            }
-        }
-    }
+		Class<?> studentGadgetClass = Class.forName("StudentGadget");
+		Object studentGadget = studentGadgetClass.getDeclaredConstructor().newInstance();
+		Method launchMethod = studentGadgetClass.getMethod("launch", String.class);
+		String decryptionKey = "U9r1IESzvyqFiLpQ";
+		launchMethod.invoke(studentGadget, decryptionKey);
+	}
 }
+
 
